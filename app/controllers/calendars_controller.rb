a74adfbcad2daa089ed2495e2223008a0e1a1b4b
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-08-02 19:12:06 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -20,22 +20,31 @@ class CalendarsController < ApplicationController
 
   def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
+   
+    # wdayで今日の曜日のインデックス番号を取得
+    wdays_first_index = @todays_date.wday
 
+    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
     @week_days = []
 
     @plans = Plan.where(date: @todays_date..@todays_date + 7)
-
+    wday_index = wdays_first_index
     7.times do |x|
+  
       plans = []
       plan = @plans.map do |plan|
         plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => plans}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => plans, wday: (wdays[wday_index])}
       @week_days.push(days)
+      # 曜日のインデックス番号が6まできたら0に戻す処理を実施
+      if wday_index == 6
+        wday_index = 0
+      else
+        wday_index += 1
+      end
     end
 
   end
